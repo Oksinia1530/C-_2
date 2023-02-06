9fd21967b8422028cd657151b88b6214f48c8d69
«Oksana <averinaoy@gmail.com>
2023-02-06 12:02:37 +0500

    Task004

---
+++ task004/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход
+число и проверяет, кратно ли оно одновременно
+7 и 23.*/
+
+Console.Clear();
+
+Console.WriteLine("Enter A");
+int A = int.Parse(Console.ReadLine()!);
+
+
+if  (A % 7 == 0 && A % 23 == 0)
+{
+
+    Console.WriteLine("YES");
+
+}
+else 
+{
+
+    Console.WriteLine("No");
+
+}
