63f5cb0cab6c9fd513f0e0dc52bad67db97ec8a3
«Oksana <averinaoy@gmail.com>
2023-02-08 11:03:26 +0500

    Kotitehtävä0089

---
+++ Kotitehtävä002/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая выводит третью
+цифру заданного числа или сообщает,
+что третьей цифры нет.*/
+
+Console.Clear();
+
+Console.WriteLine("Enter a number ");
+int A = int.Parse(Console.ReadLine()!);
+int A2 = A % 10;
+if (A2 > 0)
+{
+Console.WriteLine ($"{A2}");
+}
+else if (A2 < 0)
+{
+    Console.WriteLine ("No");
+}
